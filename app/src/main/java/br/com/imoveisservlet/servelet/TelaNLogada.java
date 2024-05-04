971165732283fe9d53217eb40364ae2c7a91bbfb
Emanuel Nabarrete <emanuelnabarretedesouza@gmail.com>
2024-05-03 21:52:05 -0300

    Tela nao logada

---
+++ app/src/main/java/br/com/imoveisservlet/servelet/TelaNLogada.java
@@ -0,0 +1,21 @@
+package br.com.imoveisservlet.servelet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("telanlogada")
+
+public class TelaNLogada {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        req.getRequestDispatcher("telanaoLogada.html").forward(req, resp);
+
+    }
+}
